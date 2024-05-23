729daacf640dada3bbcbb42a2eb5c5e268e57819
Dmitry Nasonov <gvardopo4ta@gmail.com>
2024-05-23 23:51:37 +0300

    add objects and data

--- src/mk_eb_html_tab.py
+++ src/mk_eb_html_tab.py
@@ -10,13 +10,12 @@ objs = []
 for mnum in (4, 5, 6, 8, 11, 15, 16, 17, 18, 19, 20, 21, 23, 24):
     objs.append(f"minkovskiy{mnum}")
 objs.extend((
-    "gusev4", "18abwvvvw",
-    "cz1850",
-    "c79-0", "19acdncga", "m1399", "nsv18979", "nsv13073", "nsv12175", "m1356",
-    "m1366", "cz517", "c344-18", "18aaaawyw", "c210-16",
+    "gusev4", "18abwvvvw", "cz1850", "c79-0", "19acdncga", "m1399", "nsv18979",
+    "nsv13073", "nsv12175", "m1356", "m1366", "cz517", "c344-18", "18aaaawyw",
+    "c210-16",
     "c257-37",
     "c111-36", "m1338", "nsv1409", "c180-34", "nsv18927",
-    "dde2", "nsv61", "18acrvwcz", "a293-29", "g16ams",
+    "dde2", "nsv61", "18acrvwcz", "a293-29", "g16ams", "18aagibwi",
     "u170", "m1398", "u248199", "navl02", "svkv70", "u248",
     "dde65", "u240", "u222", "n2fu0", "dde58", "dde60",
     ))
