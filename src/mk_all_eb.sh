0ffe137c7aeb91f5b88dbec9d9d118676ade204a
Dmitry Nasonov <gvardopo4ta@gmail.com>
2024-11-28 13:49:26 +0300

    update object lists

--- src/mk_all_eb.sh
+++ src/mk_all_eb.sh
@@ -1,34 +1,37 @@
 lcdir="ebecc"
 mkdir -p ../lc/$lcdir
 
-fnamecc="g194 g523 g261 18abwvvvw nsv14401 g505 m1396 g176 m1404 cz1850 g182 c79-0"\
-" 19acdncga m1399 nsv18979 nsv12175 m1356 m1366 cr1029 g163 v338lac g1829 c235-13"\
-" g187 c344-18 18aaaawyw g219 oxdel g179 c210-16 g460 cz1848 18aceoesf m1397 c257-37"\
-" cz1156 cz115 cz1153 g452 c132-17 c336-2 g217 18aabpshf 18aainzhi c88-0 g684 c111-36"\
-" g204 m1338 g220 gsc0219 cz1847 g956 18abuksln nsv1409 g333 nsv2702 c311-3 c180-34"\
-" nsv18927 nsv18857 c256-24 m1393 g434 g021-005 gds280-4 m1349 nsv61 nsv19039 m1374"\
+fnamecc="g194 g523 g261 gsc02874 18abwvvvw nsv14401 g505 m1396 cr0629 cr0640 sao25077"\
+" g176 m1404 v1403cyg cz1850 g182 cz701 cr0622 c79-0 19acdncga v1166tau bst0730 m1399 nsv18979"\
+" nsv12175 m1356 m1366 cr1029 g163 v338lac g1829 gds1549 c235-13 g187 cr0630 c344-18 18aaaawyw"\
+" gsc04267 g219 oxdel g179 c210-16 bst0731 gds0722 g460 cz1848 18aceoesf m1397 c257-37 cz1156"\
+" hd180641 cz115 cz1153 g452 c132-17 c336-2 g217 18aabpshf 18aainzhi c88-0 g684 c111-36"\
+" g204 m1338 g220 gsc0219 cz1847 g956 hd49241 18abuksln cr0849 nsv1409 g333 nsv2702 c311-3 c180-34"\
+" nsv18927 nsv18857 c256-24 m1393 g434 g965 g021-005 gds280-4 m1349 nsv61 nsv19039 gds0705 m1374"\
 " nsv18898 nsv14698 m1398 nsv3760 nsv8389 nsv3749 nsv3481"
 
-fnameccref=" koi7056 tulyn mgab282 v1123cas nsv14400 ewcru"
+fnameccref=" koi7056 tulyn mgab282 wsp12-29 v1123cas nsv14400 ewcru"
 
-for nam in $fnamecc+=$fnameccref
+for nam in $fnamecc $fnameccref
 do
+  echo $nam $lcdir/
   python plot_merged_phased_data.py $nam $lcdir/ -loaz
-  python plot_hlsp_data.py $nam $lcdir/ -lz
+  # python plot_hlsp_data.py $nam $lcdir/ -lz
 done
 
 
 lcdir="eb"
 mkdir -p ../lc/$lcdir
 
-fnameb="v698cyg ztf356-58 nsv13073 v476lac cz517 cz250 g109 v374cyg c350-33 as357-18"\
-" c65-1 cz1826 n171549 c349-7 17aadnujm cz501 g2613 n637-68 nsv18968 nsv5437 ptf5-6432 dde2"\
-" cz1225 da240 cz2494 nsv4595 17aabgexm brh160 18abryisf 18acrvwcz a293-29 g16ams 18aagibwi"\
-" u170 18abqadhy da243 u248199 navl02 svkv70 u248 dde65 c23-13 u240 u222 n2fu0"
+fnameb="gds0644 v698cyg hd10220 epic0603 gds0852 hngem ztf356-58 nsv13073 cr3573 v476lac cz517"\
+" cz250 nsv17180 g109 v374cyg nsv17721 c350-33 as357-18 tyc3983 c65-1 17aablgmo gds0754 cz1826"\
+" n171549 cz501 c349-7 17aadnujm cz257 g2613 n637-68 nsv18968 nsv5437 ptf5-6432 dde2 cz1225 cz127"\
+" da240 cz2494 nsv4595 17aabgexm brh160 18abryisf 18acrvwcz a293-29 g16ams c248-3 18aagibwi u170"\
+" 18abqadhy da243 u248199 navl02 svkv70 u248 dde65 sd1443 z0308 c23-13 u240 u222 nada99 n2fu0 g986 sd0344"
 
 fnamdde=" dde58 dde60 dde62 dde78 dde80 dde74 dde176 dde203"
 
-for nam in $fnameb+=$fnamdde
+for nam in $fnameb $fnamdde
 do
   python plot_merged_phased_data.py $nam $lcdir/ -loaz
   python plot_hlsp_data.py $nam $lcdir/ -lz
